91ed35e5c8cac40852f7e740d55d053dc18f5b6d
Vipul Chhabra <vipulchhabra555@gmail.com>
2019-09-04 12:15:33 +0530

    input output redirection completed

--- commands.c
+++ commands.c
@@ -44,123 +44,15 @@
 
 #include "main.h"
 
+#include "io_redirections.h"
+
 
 extern int low;
 extern int high;
 
 extern pid_t process[100];
 
-void stream_processing(struct arr command){
 
-        pid_t wpid;
-        int last_output = 0;
-        int status = 0;
-        int last_input = 0;
-        int append = 0;
-        int input = 0,output = 0;
-        int i = 0;
-        int command_pointer = 0;  
-           
-        char *args[100];
-        args[0] = "\0";
-        
-        while(i < 100){
-                if(strlen(command.arr[i]) == 0 )
-                        break;
-
-                else{
-                        
-                        command.arr[i][strcspn(command.arr[i],"\n")] = 0;
-                                
-                        if(strcmp(command.arr[i],">") == 0) {
-                                last_output = i;
-                                output = 1; 
-                                i += 2;     
-                        }
-
-                        else if(strcmp(command.arr[i],"<") == 0) {
-                                last_input = i;
-                                input = 1;
-                                i += 2;
-                        }
-
-                        else if(strcmp(command.arr[i],">>") == 0){
-                                last_output = i;
-                                output = 1;
-                                append = 1;
-                                i+=2;
-                        }
-
-                        else{
-                                args[command_pointer] = command.arr[i];
-                                i++;
-                                command_pointer++;
-                        }
-                                
-                }
-
-                        
-        }
-
-        command.arr[last_output+1][strcspn(command.arr[last_output+1],"\n")] = 0;
-        command.arr[last_input+1][strcspn(command.arr[last_input+1],"\n")] = 0;
-
-        if(output == 1 && input == 0 && append == 0) {
-                if(fork() == 0){
-                        int fd = open(command.arr[last_output+1],O_CREAT|O_WRONLY| O_TRUNC, 0644);
-                        dup2(fd,1);
-                        execvp(args[0],args);
-                        exit(0);
-                }
-        }
-        
-        else if(input == 1 && output == 0 && append == 0) {
-                if(fork() == 0){
-                        int fd = open(command.arr[last_input+1],O_RDONLY,0644);
-                        dup2(fd,0);
-                        execvp(args[0],args);
-                        close(fd);
-                        exit(0);
-                }
-        }
-
-        else if(input == 1 && output == 1 && append == 0){
-                if(fork() == 0){
-                        int fd = open(command.arr[last_input+1],O_RDONLY,0644);
-                        int fd2 = open(command.arr[last_output+1],O_CREAT|O_WRONLY|O_TRUNC, 0644);
-                        dup2(fd2,1);
-                        dup2(fd,0);
-                        execvp(args[0],args);
-                        close(fd);
-                        exit(0);
-                }
-        }
-
-        if(output == 1 && input == 0 && append == 1) {
-                if(fork() == 0){
-                        int fd = open(command.arr[last_output+1],O_WRONLY|O_APPEND,0644);
-                        dup2(fd,1);
-                        execvp(args[0],args);
-                        exit(0);
-                }
-        }
-        
-
-        else if(input == 1 && output == 1 && append == 1){
-                if(fork() == 0){
-                        int fd = open(command.arr[last_input+1],O_RDONLY,0644);
-                        int fd2 = open(command.arr[last_output+1],O_CREAT|O_WRONLY|O_APPEND, 0644);
-                        dup2(fd2,1);
-                        dup2(fd,0);
-                        execvp(args[0],args);
-                        close(fd);
-                        exit(0);
-                }
-        }
-        
-
-         
-}
 
 void cd_command(struct arr command) {
         int x = chdir(command.arr[1]);
---
+++ io_redirections.h
@@ -0,0 +1,7 @@
+#ifndef IO_REDIRECTIONS_H 
+#define IO_REDIRECTIONS_H
+
+struct arr;
+void stream_processing(struct arr command);
+
+#endif
